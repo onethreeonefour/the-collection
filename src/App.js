f5fabe83bac230ad2cc35e8d11f5495b3ff4f7e5
Terry Nguyen <plow-king@hotmail.com>
2021-03-04 11:41:00 +1100

    remove old code

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import React from "react";
 import { Route } from "react-router-dom";
 import NavBar from "./Components/Navigation/Navbar";
 import Landing from "./Components/Landing/Landing";
-import Footer from "./Components/Navigation/Footer";
 
 function App() {
   return (
